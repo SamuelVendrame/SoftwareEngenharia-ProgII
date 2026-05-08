a6d04fd94707ccccdf0b3de12e2edad7052155cc
William Bomfim <williambomfim99@gmail.com>
2026-05-08 20:08:34 -0300

    Commit inicial com as constantes definidas.

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val listaPolegadasLonga = listOf("1","3","6","9","1","1,50","2","3","4","5","6","7","8","10")
+    val listaPolegadasCurta = listOf("3","6","9","1","1,50","2","3","4","5")
+    val listaColunaW_CM = listOf(2.5,7.6,15.2,22.9,30.5,45.7,61,91.5,122,152.5,183,213.5,244,305)
+    val listaColunaE = listOf(22.9,38.1,45.7,61,91.5,91.5,91.5,91.5,91.5,91.5,91.5,91.5,91.5,122)
+    val listVazaoMinima = listOf(0.85,1.52,2.55,3.11,4.25,11.89,17.26,36.79,62.8)
+    val listVazaoMaxima = listOf(53.8,110.4,251.9,455.6,696.2,936.7,1426,1921,2422)
+    val listaColunaK = listOf(0.1771,0.3812,0.5354,0.6909,1.056,1.429,2.184,2.963,3.732)
+    var listaColunaN = listOf(1.5447,1.53,1.53,1.522,1.538,1.55,1.5666,1.5738,1.587)
+    println("Insira a população da cidade")
+    //val name = readln().toDouble()
+
+    val map = hashMapOf("3" to 0.1771, "6" to 0.3812)
+    map.get("3")
+}
