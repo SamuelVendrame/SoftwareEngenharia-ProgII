19f37a13cf1c2eee8480b02c9003701396a11dd8
William Bomfim <williambomfim99@gmail.com>
2026-05-11 16:51:27 -0300

    Terminado as funções de calculo, e feita uma separação com os três item mais proximos de 70%.

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,9 @@
 import kotlin.coroutines.CoroutineContext
 import kotlin.math.pow
 
-val listaPolegadasLonga = listOf("1","3","6","9","1","1,50","2","3","4","5","6","7","8","10")
-val listaPolegadasCurta = listOf("3","6","9","1","1,50","2","3","4","5")
+//Na moral, QUEM inventou de colocar aspas em polegada??????
+val listaPolegadasLonga = listOf("1\"","3\"","6\"","9\"","1\'","1,50\'","2\'","3\'","4\'","5\'","6\'","7\'","8\'","10\'")
+val listaPolegadasCurta = listOf("3\"","6\"","9\"","1\'","1,50\'","2\'","3\'","4\'","5\'")
 val listaColunaW_CM = listOf<Double>(2.5,7.6,15.2,22.9,30.5,45.7,61.0,91.5,122.0,152.5,183.0,213.5,244.0,305.0)
 val listaColunaE = listOf<Double>(22.9,38.1,45.7,61.0,91.5,91.5,91.5,91.5,91.5,91.5,91.5,91.5,91.5,122.0)
 val listVazaoMinima = listOf<Double>(0.85,1.52,2.55,3.11,4.25,11.89,17.26,36.79,62.8)
@@ -19,17 +20,51 @@ val colunaN: Map<String,Double> = listaPolegadasCurta.zip(listaColunaN).toMap<St
 
 fun main() {
     val habitantes: Int
-    val consumoAguaHabitanteDia = 160 //Dado estimado hardcoded
-    habitantes = 70000
+    val consumoAguaHabitanteDia = 150 //Dado estimado hardcoded
+    habitantes = 90000
 
     val vasaoLitrosPorDia = habitantes * consumoAguaHabitanteDia
     val vasaoLitrosPorSegundo = vasaoLitrosPorDia/86400.0
 
-    for(polegada in listaPolegadasCurta) {
-        //TODO: Ta fora de ordem, MAS ATENCAO: pra mudar a ordem de uma das listas constantes tem que mudar de todas.
-        println("para a cm: ${colunaW_CM.get(polegada)}, a vazão é de ${calcularCalhaDeParshall(vasaoLitrosPorSegundo,polegada)}%")
+    val listaCalculado = mutableListOf<Pair<String,Int>>()
+    for(polegada in listaPolegadasCurta ){
+        listaCalculado.add(Pair(polegada,calcularCalhaDeParshall(vasaoLitrosPorSegundo,polegada)))
     }
 
+    val valoresProximos = mutableListOf<Pair<String,Int>>()
+    for(index in listaCalculado.indices){
+        if(listaCalculado[index].second < 70){
+            if(index == listaCalculado.size - 1){
+                valoresProximos.add(listaCalculado[index-2])
+            }
+            if(index != 0) {
+                valoresProximos.add(listaCalculado[index - 1])
+            }
+            valoresProximos.add(listaCalculado[index])
+            if(index != listaCalculado.size - 1) {
+                valoresProximos.add(listaCalculado[index + 1])
+            }
+            if(index == 0){
+                valoresProximos.add(listaCalculado[index+2])
+            }
+            break
+        }
+    }
+
+    for(item in valoresProximos){
+        if(item.second < 70) {
+            println("\u001B[34m${item.first}\u001B[0m para a cm: \u001B[34m${colunaW_CM.get(item.first)}\u001B[0m, a vazão é de \u001B[32m${item.second}%\u001B[0m")
+        }else{
+            println("\u001B[34m${item.first}\u001B[0m para a cm: \u001B[34m${colunaW_CM.get(item.first)}\u001B[0m, a vazão é de \u001B[31m${item.second}%\u001B[0m")
+        }
+
+    }
+    if(valoresProximos.isEmpty()){
+        println("NAO FOI POSSIVEL CALCULAR NENHUM VALOR ABAIXO DE 70%")
+        for(item in listaCalculado){
+            println("${item.first} para a cm: ${colunaW_CM.get(item.first)}, a vazão é de ${item.second}%")
+        }
+    }
 
 }
 
